9e8a3c800a480f69d3aad4375d865f7ff437bd86
KnowledgeNomad <128027915+KnowledgeNomad@users.noreply.github.com>
2024-11-03 23:52:06 +0530

    Create arraycount.c

---
+++ arraycount.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Number of elements in the array: %d\n", n);
+    return 0;
+}
